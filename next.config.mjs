d4645fe59e7a5e5108bad486cebe1424d4102159
Shiv Kohli <162525184+ShivKohli@users.noreply.github.com>
2025-10-21 20:16:19 -0400

    Configure webpack alias in next.config.mjs
    Added webpack alias configuration for '@' imports.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    // Add alias for "@/..." imports
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
   experimental: {
-    serverActions: { allowedOrigins: ['*'] }
-  }
+    serverActions: { allowedOrigins: ["*"] },
+  },
 };
+
 export default nextConfig;
