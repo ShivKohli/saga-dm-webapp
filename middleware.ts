34a78b6b5ae39b1b6d0ac7d0d77afe315705e1bd
Shiv Kohli <162525184+ShivKohli@users.noreply.github.com>
2025-10-24 00:39:44 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,13 @@ import { createMiddlewareClient } from "@supabase/ssr";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-
-  // Create a Supabase client that can read auth cookies
   const supabase = createMiddlewareClient({ req, res });
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  // Protect /play
+  // Protect /play route
   if (!session && req.nextUrl.pathname.startsWith("/play")) {
     const redirectUrl = req.nextUrl.clone();
     redirectUrl.pathname = "/";
